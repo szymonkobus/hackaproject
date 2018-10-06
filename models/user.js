0233bd625baa7b224d613754cea55f3e97a1a280
szymonkobus <36537361+szymonkobus@users.noreply.github.com>
2018-10-06 17:02:36 +0100

    querring

--- models/user.js
+++ models/user.js
@@ -3,30 +3,33 @@ var mongoose = require('mongoose');
 
 var db = mongoose.connection;
 
-console.log("User model created");
+var us = new mongoose.Schema({
+    name: String,
+    pass: String
+})
+
+var User = mongoose.model('user', us);
 
 mongoose.connect("mongodb+srv://admin:admin123@cluster0-tyvbl.gcp.mongodb.net/test",{ useNewUrlParser: true}, function(err, client) {
-   // perform actions on the collection object
-   if(err) {
-     console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-   }
-
+   if(err) { console.log('Error occurred while connecting to MongoDB Atlas...\n',err); }
    console.log('Connected...');
-   const collection = db.collection("inventory");
-
-   // perform actions on the collection object
-   db.collection("inventory").insertMany([
-   // MongoDB adds the _id field with an ObjectId if _id is not present
-   { item: "test", qty: 25, status: "A",
-       size: { h: 14, w: 21, uom: "cm" }, tags: [ "blank", "red" ] },
-   { item: "test2", qty: 50, status: "A",
-       size: { h: 8.5, w: 11, uom: "in" }, tags: [ "red", "blank" ] },
-   { item: "test3", qty: 100, status: "D",
-       size: { h: 8.5, w: 11, uom: "in" }, tags: [ "red", "blank", "plain" ] }
-  ])
-  .then(function(result) {
-  // process result
-  })
-
-   client.close();
 });
+
+//adduser("Tom","123");
+
+//querring
+User.find({name: "Mike"}).exec(function(err, docs){
+  if(err) throw err;
+  console.log();
+  console.log(docs);
+});
+
+
+//client.close();
+
+function adduser(_name, _password){
+  var temp_user = new User({name: _name, pass: _password});
+  temp_user.save(function(err){
+    if(err) console.log("error: user not saved")
+  });
+}
